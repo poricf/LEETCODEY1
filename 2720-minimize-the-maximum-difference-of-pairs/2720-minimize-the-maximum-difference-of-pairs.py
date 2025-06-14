f0952369460024cfba2a6b85b6aec3b10f2a8aed
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2025-06-14 13:58:30 +0300

    Time: 371 ms (71.26%), Space: 32.6 MB (87.01%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.py
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.py
@@ -1,27 +1,31 @@
-from typing import List
-
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
-        n = len(nums)
 
-        def can_form(max_diff):
-            count = 0
+        def check(mid):
             i = 1
-            while i < n:
-                if nums[i] - nums[i - 1] <= max_diff:
-                    count += 1
-                    i += 2  # use both elements, skip next
+            cnt = 0
+            while i < len(nums):
+                if nums[i] - nums[i - 1] <= mid:
+                    i += 2
+                    cnt += 1
                 else:
                     i += 1
-            return count >= p
+            
+            return cnt >= p
+        
 
-        left, right = 0, nums[-1] - nums[0]
-        while left < right:
+        left = 0
+        right = nums[-1] - nums[0]
+        ans = float("inf")
+        while left <= right:
             mid = (left + right) // 2
-            if can_form(mid):
-                right = mid
+            if check(mid):
+                ans = min(mid , ans)
+                right = mid - 1
             else:
                 left = mid + 1
+       
+        return ans
+                
 
-        return left
