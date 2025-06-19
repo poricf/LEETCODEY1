059026c8a83dffe52f18ed4dc813cc62688d2eaa
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2025-06-20 05:57:26 +0900

    Time: 395 ms (90.61%), Space: 17.9 MB (91.26%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -3,15 +3,16 @@ class Solution:
         # dp[i][j] -> the longset subsequence in t[:i] s[:j]
         M = len(s) + 1
         N = len(t) + 1
-        dp = [[0] * M for _ in range(N)]
+        prev = [0] * M
+        cur = [0] * M
 
         for i in range(1 , N):
             for j in range(1 , M):
                 if s[j - 1] == t[i - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    cur[j] = prev[j - 1] + 1
                 else:
-                    without_s = dp[i][j - 1]
-                    without_t = dp[i - 1][j]
-                    dp[i][j] = max(without_s , without_t)
+                    cur[j] = max(prev[j] , cur[j - 1])
+
+            prev , cur = cur , [0] * M
         
-        return dp[-1][-1]
+        return prev[-1]
