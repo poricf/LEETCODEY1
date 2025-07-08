004ab6e45c86df1c34e1c6951027f357f0ad518c
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2025-07-08 17:43:55 -0230

    Time: 969 ms (49.28%), Space: 97.1 MB (52.63%) - LeetHub

---
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,32 @@
+from bisect import bisect_right
+
+class Solution:
+    def maxValue(self, events, k):
+        events.sort(key=lambda x: x[1])
+        n = len(events)
+
+        start_times = [e[0] for e in events]
+
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            start, end, value = events[i - 1]
+
+            prev = self.find(events, i - 1, start)
+
+            for j in range(1, k + 1):
+                dp[i][j] = max(dp[i - 1][j], dp[prev + 1][j - 1] + value)
+
+        return dp[n][k]
+
+    def find(self, events, right, targetStart):
+        left = 0
+        res = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if events[mid][1] < targetStart:
+                res = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
